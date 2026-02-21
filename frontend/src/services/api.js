07f132fdf0408f8ecd042742124041908e2a0098
AKKI <2300030796@kluniversity.in>
2026-02-22 04:27:14 +0530

    env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const BASE_URL = '/api'
+// const BASE_URL = '/api'
+
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api'
+
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
